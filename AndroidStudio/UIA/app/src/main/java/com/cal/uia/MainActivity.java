afb464ff0c6594135408e39a3b6b49bd3401346a
wasabitinn <wasabitinn@yahoo.com.tw>
2017-07-27 16:05:25 +0800

    add addroid  project

---
+++ AndroidStudio/UIA/app/src/main/java/com/cal/uia/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cal.uia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,10 +1,10 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
-
+using Android.Content;
 namespace Jessie.Droid
 {
-    [Activity(Label = "Jessie", MainLauncher = true, Icon = "@mipmap/icon")]
+    [Activity(Label = "MasterActivity", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
         int count = 1;
@@ -14,13 +14,17 @@ namespace Jessie.Droid
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main); //把controler 與View link
 
             // Get our button from the layout resource,
             // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.myButton);
 
-            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+            button.Click += delegate
+            {
+                var intent = new Intent(this, typeof(Mainflow.MasterActivity));
+                StartActivity(intent);
+            };
         }
     }
 }
